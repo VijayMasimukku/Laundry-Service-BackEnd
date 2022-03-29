f9b1ace7b41bed1d4ae1fb857d34bd140502e124
VijayMasimukku <vijayps4223@gmail.com>
2022-03-29 20:58:38 +0530

    final-backend

---
+++ index.js
@@ -0,0 +1,33 @@
+
+const express = require('express')
+const app =express()
+
+
+const mongoose = require('mongoose')
+//connecting to DB
+ mongoose.connect("mongodb+srv://Team15:team15@cluster0.yirhq.mongodb.net/laundry?retryWrites=true&w=majority",{ useNewUrlParser: true }).then( () => console.log("MongoDB Connected")).catch( (err) => console.log("MongoDB error"))
+
+
+//external middleware
+console.log("mongosee connected");
+const cors = require('cors')
+app.use(cors())
+
+//built-in middleware
+app.use(express.json())
+
+
+//user routes
+app.use( require("./routes/auth"))
+
+//orders routes
+app.use("/order" ,require("./routes/orders"))
+
+
+const PORT= 5000
+
+//creating server
+app.listen(PORT,()=>{
+    console.log("app is listing at port 5000");
+})
+
